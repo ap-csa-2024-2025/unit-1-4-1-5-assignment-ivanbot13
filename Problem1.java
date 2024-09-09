285e6d4ca04cfab404d1462f67552a442da4e32b
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-09 11:01:08 -0700

    prob1

--- Problem1.java
+++ Problem1.java
@@ -2,7 +2,19 @@ public class Problem1
 {
   public static void main(String[] args)
   {
- 
+    int num = 1;
+    System.out.println("The inital value is " + num);
+
+    for (int i = 0; i < 4; i++) {
+      num++;
+      System.out.println("The number is now: " + num);
+    }
+
+    for (int i = 0; i < 4; i++) {
+      num--;
+      System.out.println("The number is now: " + num);
+    }
+    
   }
 }
 
