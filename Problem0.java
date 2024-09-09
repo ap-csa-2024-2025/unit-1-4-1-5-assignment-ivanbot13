95a21d4dccbb9d09f389f0f24dea55b47f3e68c5
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-09 10:45:48 -0700

    class

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,12 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double num = 1;
+    System.out.println(num);
+    //it runs
+    int num2 = 2.0;
+    System.out.println(num2);
+    //doesnt run
+    //its possible to store an int into a double but not a double into an int
   }
 }
