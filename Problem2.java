7366e03473f04afe8772822ae4d327f973caa2f2
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-09 11:14:56 -0700

    2nd.prob.done

--- Problem2.java
+++ Problem2.java
@@ -2,7 +2,17 @@ public class Problem2
 {
   public static void main(String[] args)
   {
- 
+    int num = 987;
+    System.out.println("Three digit number: " + num);
+
+    int firDigit = num/100;
+    int secDigit = (num/10)%10;
+    int thrDigit = num%10;
+    
+    System.out.println("Here are the digits:");
+    System.out.println(firDigit);
+    System.out.println(secDigit);
+    System.out.println(thrDigit);
   }
 }
 
